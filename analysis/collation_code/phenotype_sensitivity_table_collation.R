9869224f182fc1eee0f411fa270fca911257c4fd
Em Prestige <emprest97@gmail.com>
2024-08-16 18:02:04 +0100

    add sensitivity analysis to explore phenotype sensitivity

---
+++ analysis/collation_code/phenotype_sensitivity_table_collation.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(here)
+library(arrow)
+library(ggplot2)
+
+#define cohort
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 0) {
+  cohort <- "adults"
+} else {
+  cohort <- args[[1]]
+}
+
+## create output directories ----
+fs::dir_create(here("output", "collated", "descriptive"))
+
+##phenotype sensitivity
+
+# import phenotype sensitivity by cohort
+collated_phenotype_sensitivity = rbind(
+  read_csv(here::here("output", "sensitivity", paste0("phenotype_sensitivity_", 
+           cohort, "_2016_2017.csv"))) %>% mutate(subset = "2016_17"),
+  read_csv(here::here("output", "sensitivity", paste0("phenotype_sensitivity_", 
+           cohort, "_2017_2018.csv"))) %>% mutate(subset = "2017_18"),
+  read_csv(here::here("output", "sensitivity", paste0("phenotype_sensitivity_", 
+           cohort, "_2018_2019.csv"))) %>% mutate(subset = "2018_19"),
+  read_csv(here::here("output", "sensitivity", paste0("phenotype_sensitivity_", 
+           cohort, "_2019_2020.csv"))) %>% mutate(subset = "2019_20"),
+  read_csv(here::here("output", "sensitivity", paste0("phenotype_sensitivity_", 
+           cohort, "_2020_2021.csv"))) %>% mutate(subset = "2020_21"),
+  read_csv(here::here("output", "sensitivity", paste0("phenotype_sensitivity_", 
+           cohort, "_2021_2022.csv"))) %>% mutate(subset = "2021_22"),
+  read_csv(here::here("output", "sensitivity", paste0("phenotype_sensitivity_", 
+           cohort, "_2022_2023.csv"))) %>% mutate(subset = "2022_23")
+)
+
+#save as csv
+write_csv(collated_phenotype_sensitivity, paste0(here::here("output", "collated", "descriptive"), 
+          "/", cohort, "_phenotype_sensitivity_collated.csv"))
--- analysis/sensitivity_analyses/phenotype_sensitivity.R
+++ analysis/sensitivity_analyses/phenotype_sensitivity.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(here)
 library(arrow)
+library(lubridate)
 
 ## create output directories ----
 fs::dir_create(here("analysis"))
@@ -9,13 +10,13 @@ fs::dir_create(here("analysis"))
 source(here("analysis", "design", "design.R"))
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) == 0) {
-  study_start_date <- "2022-09-01"
-  study_end_date <- "2023-08-31"
-  cohort <- "adults"
+  study_start_date <- "2019-09-01"
+  study_end_date <- "2020-08-31"
+  cohort <- "infants"
 } else {
+  cohort <- args[[1]]
   study_start_date <- study_dates[[args[[2]]]]
   study_end_date <- study_dates[[args[[3]]]]
-  cohort <- args[[1]]
 }
 covid_season_min <- as.Date("2019-09-01")
 covid_current_vacc_min = as.Date("2020-09-01", "%Y-%m-%d")
@@ -135,7 +136,7 @@ if (study_start_date >= covid_season_min) {
   )
 }
 
-#for comparisons with overall resp outcome
+#for comparisons with overall resp outcome mild
 if (study_start_date >= covid_season_min) {
   df_input_sensitive$mild_combined_overall = paste0(
     df_input_sensitive$rsv_mild_alt, "_",
@@ -150,3 +151,72 @@ if (study_start_date >= covid_season_min) {
     df_input_sensitive$overall_resp_mild_alt
   )
 }
+
+#for comparisons with overall resp outcome severe
+if (study_start_date >= covid_season_min) {
+  df_input_sensitive$severe_combined_overall = paste0(
+    df_input_sensitive$rsv_severe_alt, "_",
+    df_input_sensitive$flu_severe_alt, "_",
+    df_input_sensitive$covid_severe_alt, "_",
+    df_input_sensitive$overall_resp_severe_alt
+  )
+} else {
+  df_input_sensitive$severe_combined_overall = paste0(
+    df_input_sensitive$rsv_severe_alt, "_",
+    df_input_sensitive$flu_severe_alt, "_",
+    df_input_sensitive$overall_resp_severe_alt
+  )
+}
+
+#count number of patients in each category for specific phenotypes - separately for mild and severe 
+patients_specific_mild <- rlang::duplicate(df_input_specific) %>% 
+  group_by("combo" = mild_combined) %>%
+  summarise(n = n()) %>%
+  ungroup() %>%
+  mutate(outcome_type = "mild", codelist_type = "specific")
+patients_specific_severe <- rlang::duplicate(df_input_specific) %>%
+  group_by("combo" = severe_combined) %>%
+  summarise(n = n()) %>%
+  ungroup() %>% 
+  mutate(outcome_type = "severe", codelist_type = "specific")
+patients_specific <- full_join(patients_specific_mild, patients_specific_severe)
+
+#count number of patients in each category for sensitive phenotypes - separately for mild and severe
+patients_sensitive_mild <- rlang::duplicate(df_input_sensitive) %>% 
+  group_by("combo" = mild_combined_overall) %>%
+  summarise(n = n()) %>%
+  ungroup() %>%
+  mutate(outcome_type = "mild", codelist_type = "sensitive")
+patients_sensitive_severe <- rlang::duplicate(df_input_sensitive) %>%
+  group_by("combo" = severe_combined_overall) %>%
+  summarise(n = n()) %>%
+  ungroup() %>% 
+  mutate(outcome_type = "severe", codelist_type = "sensitive")
+patients_sensitive <- full_join(patients_sensitive_mild, patients_sensitive_severe)
+
+#count number of patients in each category for sensitive phenotypes now including overall resp - separately for mild and severe
+patients_sensitive_mild_overall <- rlang::duplicate(df_input_sensitive) %>% 
+  group_by("combo" = mild_combined_overall) %>%
+  summarise(n = n()) %>%
+  ungroup() %>%
+  mutate(outcome_type = "mild_overall", codelist_type = "sensitive")
+patients_sensitive_severe_overall <- rlang::duplicate(df_input_sensitive) %>%
+  group_by("combo" = severe_combined_overall) %>%
+  summarise(n = n()) %>%
+  ungroup() %>% 
+  mutate(outcome_type = "severe_overall", codelist_type = "sensitive")
+patients_sensitive_overall <- full_join(patients_sensitive_mild_overall, 
+                                        patients_sensitive_severe_overall)
+
+#combine as one tibble 
+patients_combined <- patients_specific %>%
+  full_join(patients_sensitive) %>%
+  full_join(patients_sensitive_overall)
+
+## create output directories ----
+fs::dir_create(here("output", "sensitivity"))
+
+#write to file
+write_csv(patients_combined, paste0(here::here("output", "sensitivity"),
+          "/", "phenotype_sensitivity_", cohort, "_", year(study_start_date), "_", 
+          year(study_end_date), ".csv"))
