f9207b78e15dd81b77cf8237ecd8f51430844544
Em Prestige <emprest97@gmail.com>
2024-09-24 12:05:00 +0100

    update yaml

--- create-project.R
+++ create-project.R
@@ -885,22 +885,11 @@ action_descriptive <- function(cohort, season, dates, codelist_type,
   splice(
     
     action(
-      name = glue("process_dataset_{cohort}_{season}_sensitive_secondary"),
-      run = glue("r:latest analysis/data_processing.R {cohort} {season_start_date} {season_end_date} sensitive secondary"),
-      arguments = c(cohort, season, dates, codelist_type, investigation_type, 
-                    season_start_date, season_end_date),
-      needs = list(glue("generate_dataset_{cohort}_{season}_{codelist_type}_{investigation_type}"),
-                   glue("process_household_information_{season}")),
-      highly_sensitive = lst(
-        cohort = glue("output/data/input_processed_{cohort}_{dates}_{codelist_type}_{investigation_type}.arrow"))
-    ),
-    
-    action(
       name = glue("describe_cohort_{cohort}_{season}"),
-      run = glue("r:latest analysis/cohort_description.R {cohort} {season_start_date} {season_end_date} sensitive secondary"),
+      run = glue("r:latest analysis/cohort_description.R {cohort} {season_start_date} {season_end_date} specific secondary"),
       arguments = c(cohort, season, dates, codelist_type, investigation_type, 
                     season_start_date, season_end_date),
-      needs = list(glue("process_dataset_{cohort}_{season}_sensitive_secondary")),
+      needs = list(glue("process_dataset_{cohort}_{season}_specific_secondary")),
       moderately_sensitive = lst(
         csv = glue("output/table1/table1_{cohort}_{dates}.csv")),
     )
