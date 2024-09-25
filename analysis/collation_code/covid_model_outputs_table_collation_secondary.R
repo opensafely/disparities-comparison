20d06bd162593c17a98d8ff1aacae7d5f41d3b5d
Em Prestige <emprest97@gmail.com>
2024-09-25 12:17:18 +0100

    update secondary analyses

---
+++ analysis/collation_code/covid_model_outputs_table_collation_secondary.R
@@ -0,0 +1,62 @@
+library(tidyverse)
+library(here)
+library(arrow)
+library(ggplot2)
+
+#define cohort
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 0) {
+  cohort <- "adults"
+} else {
+  cohort <- args[[1]]
+}
+
+## create output directories ----
+fs::dir_create(here("output", "collated", "secondary"))
+
+##model outputs
+
+#import covid results table by cohort
+#(secondary investigation, specific phenotypes)
+collated_model_outputs_covid = rbind(
+  read_csv(here::here("output", "results", "models", "covid_specific_secondary", 
+                      paste0("covid_ethnicity_model_outputs_", 
+                             cohort, "_2020_2021_specific_secondary.csv"))) 
+  %>% mutate(model_type = "ethnicity", codelist_type = "specific", 
+             investigation_type = "secondary", subset = "2020_21"),
+  read_csv(here::here("output", "results", "models", "covid_specific_secondary", 
+                      paste0("covid_ses_model_outputs_", 
+                             cohort, "_2020_2021_specific_secondary.csv"))) 
+  %>% mutate(model_type = "ses", codelist_type = "specific", 
+             investigation_type = "secondary", subset = "2020_21"),
+  read_csv(here::here("output", "results", "models", "covid_specific_secondary", 
+                      paste0("covid_hh_comp_model_outputs_", 
+                             cohort, "_2020_2021_specific_secondary.csv")))
+  %>% mutate(model_type = "composition", codelist_type = "specific", 
+             investigation_type = "secondary", subset = "2020_21"),
+  read_csv(here::here("output", "results", "models", "covid_specific_secondary", 
+                      paste0("covid_ethnicity_ses_model_outputs_", 
+                             cohort, "_2020_2021_specific_secondary.csv"))) 
+  %>% mutate(model_type = "ethnicity_ses", codelist_type = "specific", 
+             investigation_type = "secondary", subset = "2020_21"),
+  read_csv(here::here("output", "results", "models", "covid_specific_secondary", 
+                      paste0("covid_ethnicity_hh_comp_model_outputs_", 
+                             cohort, "_2020_2021_specific_secondary.csv"))) 
+  %>% mutate(model_type = "ethnicity_composition", codelist_type = "specific", 
+             investigation_type = "secondary", subset = "2016_17"),
+  read_csv(here::here("output", "results", "models", "covid_specific_secondary", 
+                      paste0("covid_ses_hh_comp_model_outputs_", 
+                             cohort, "_2020_2021_specific_secondary.csv"))) 
+  %>% mutate(model_type = "ses_composition", codelist_type = "specific", 
+             investigation_type = "secondary", subset = "2020_21"),
+  read_csv(here::here("output", "results", "models", "covid_specific_secondary", 
+                      paste0("covid_full_model_outputs_", 
+                             cohort, "_2020_2021_specific_secondary.csv"))) 
+  %>% mutate(model_type = "full", codelist_type = "specific", 
+             investigation_type = "secondary", subset = "2020_21")
+)
+
+#save as csv
+write_csv(collated_model_outputs_covid, 
+          paste0(here::here("output", "collated", "secondary"),
+                 "/", cohort, "_covid_model_outputs_collated_secondary.csv"))
---
+++ analysis/collation_code/flu_model_outputs_table_collation_secondary.R
@@ -0,0 +1,62 @@
+library(tidyverse)
+library(here)
+library(arrow)
+library(ggplot2)
+
+#define cohort
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 0) {
+  cohort <- "adults"
+} else {
+  cohort <- args[[1]]
+}
+
+## create output directories ----
+fs::dir_create(here("output", "collated", "secondary"))
+
+##model outputs
+
+#import flu results table by cohort
+#(secondary investigation, specific phenotypes)
+collated_model_outputs_flu = rbind(
+  read_csv(here::here("output", "results", "models", "flu_specific_secondary", 
+                      paste0("flu_ethnicity_model_outputs_", 
+                             cohort, "_2018_2019_specific_secondary.csv"))) 
+  %>% mutate(model_type = "ethnicity", codelist_type = "specific", 
+             investigation_type = "secondary", subset = "2018_19"),
+  read_csv(here::here("output", "results", "models", "flu_specific_secondary", 
+                      paste0("flu_ses_model_outputs_", 
+                             cohort, "_2018_2019_specific_secondary.csv"))) 
+  %>% mutate(model_type = "ses", codelist_type = "specific", 
+             investigation_type = "secondary", subset = "2018_19"),
+  read_csv(here::here("output", "results", "models", "flu_specific_secondary", 
+                      paste0("flu_hh_comp_model_outputs_", 
+                             cohort, "_2018_2019_specific_secondary.csv")))
+  %>% mutate(model_type = "composition", codelist_type = "specific", 
+             investigation_type = "secondary", subset = "2018_19"),
+  read_csv(here::here("output", "results", "models", "flu_specific_secondary", 
+                      paste0("flu_ethnicity_ses_model_outputs_", 
+                             cohort, "_2018_2019_specific_secondary.csv"))) 
+  %>% mutate(model_type = "ethnicity_ses", codelist_type = "specific", 
+             investigation_type = "secondary", subset = "2018_19"),
+  read_csv(here::here("output", "results", "models", "flu_specific_secondary", 
+                      paste0("flu_ethnicity_hh_comp_model_outputs_", 
+                             cohort, "_2018_2019_specific_secondary.csv"))) 
+  %>% mutate(model_type = "ethnicity_composition", codelist_type = "specific", 
+             investigation_type = "secondary", subset = "2016_17"),
+  read_csv(here::here("output", "results", "models", "flu_specific_secondary", 
+                      paste0("flu_ses_hh_comp_model_outputs_", 
+                             cohort, "_2018_2019_specific_secondary.csv"))) 
+  %>% mutate(model_type = "ses_composition", codelist_type = "specific", 
+             investigation_type = "secondary", subset = "2018_19"),
+  read_csv(here::here("output", "results", "models", "flu_specific_secondary", 
+                      paste0("flu_full_model_outputs_", 
+                             cohort, "_2018_2019_specific_secondary.csv"))) 
+  %>% mutate(model_type = "full", codelist_type = "specific", 
+             investigation_type = "secondary", subset = "2018_19")
+)
+
+#save as csv
+write_csv(collated_model_outputs_flu, 
+          paste0(here::here("output", "collated", "secondary"),
+                 "/", cohort, "_flu_model_outputs_collated_secondary.csv"))
