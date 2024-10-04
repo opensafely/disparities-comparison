15145e223c3f2d07235a5101d2b3dcab98f70aac
Em Prestige <emprest97@gmail.com>
2024-10-04 15:14:27 +0100

    Update table1_table_collation.R

--- analysis/collation_code/table1_table_collation.R
+++ analysis/collation_code/table1_table_collation.R
@@ -20,31 +20,31 @@ fs::dir_create(here("output", "collated", "descriptive"))
 collated_table1 = rbind(
   read_csv(here::here("output", "table1", paste0("table1_", cohort, 
            "_2016_2017.csv"))) %>% rename(N = 2) %>% 
-    mutate(N = ifelse(row_number() == 1, .[2, 2], N)) %>%
+    mutate(N = if_else(row_number() == 1, pull(.[2, 2]), N)) %>%
     slice(-2) %>% mutate(subset = "2016_17") ,
   read_csv(here::here("output", "table1", paste0("table1_", cohort, 
            "_2017_2018.csv"))) %>% rename(N = 2) %>% 
-    mutate(N = ifelse(row_number() == 1, .[2, 2], N)) %>%
+    mutate(N = if_else(row_number() == 1, pull(.[2, 2]), N)) %>%
     slice(-2) %>%  mutate(subset = "2017_18"),
   read_csv(here::here("output", "table1", paste0("table1_", cohort,
            "_2018_2019.csv"))) %>% rename(N = 2) %>% 
-    mutate(N = ifelse(row_number() == 1, .[2, 2], N)) %>%
+    mutate(N = if_else(row_number() == 1, pull(.[2, 2]), N)) %>%
     slice(-2) %>%  mutate(subset = "2018_19"),
   read_csv(here::here("output", "table1", paste0("table1_", cohort, 
            "_2019_2020.csv"))) %>% rename(N = 2) %>% 
-    mutate(N = ifelse(row_number() == 1, .[2, 2], N)) %>%
+    mutate(N = if_else(row_number() == 1, pull(.[2, 2]), N)) %>%
     slice(-2) %>%  mutate(subset = "2019_20"),
   read_csv(here::here("output", "table1", paste0("table1_", cohort, 
            "_2020_2021.csv"))) %>% rename(N = 2) %>% 
-    mutate(N = ifelse(row_number() == 1, .[2, 2], N)) %>%
+    mutate(N = if_else(row_number() == 1, pull(.[2, 2]), N)) %>%
     slice(-2) %>%  mutate(subset = "2020_21"),
   read_csv(here::here("output", "table1", paste0("table1_", cohort, 
            "_2021_2022.csv"))) %>% rename(N = 2) %>% 
-    mutate(N = ifelse(row_number() == 1, .[2, 2], N)) %>%
+    mutate(N = if_else(row_number() == 1, pull(.[2, 2]), N)) %>%
     slice(-2) %>%  mutate(subset = "2021_22"),
   read_csv(here::here("output", "table1", paste0("table1_", cohort, 
            "_2022_2023.csv"))) %>% rename(N = 2) %>% 
-    mutate(N = ifelse(row_number() == 1, .[2, 2], N)) %>%
+    mutate(N = if_else(row_number() == 1, pull(.[2, 2]), N)) %>%
     slice(-2) %>%  mutate(subset = "2022_23")
 )
 
