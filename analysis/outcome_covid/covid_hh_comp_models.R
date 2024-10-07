5a8b8f7e9486423f16da50e44441585a8969eac9
Em Prestige <emprest97@gmail.com>
2024-10-07 13:45:47 +0100

    update model code
    add check for sourcing

--- analysis/outcome_covid/covid_hh_comp_models.R
+++ analysis/outcome_covid/covid_hh_comp_models.R
@@ -11,19 +11,22 @@ fs::dir_create(here("analysis", "outcome_covid"))
 
 #define study start date and study end date
 source(here("analysis", "design", "design.R"))
-args <- commandArgs(trailingOnly = TRUE)
-if (length(args) == 0) {
-  study_start_date <- "2016-09-01"
-  study_end_date <- "2017-08-31"
-  cohort <- "infants"
-  codelist_type <- "sensitive"
-  investigation_type <- "primary"
-} else {
-  study_start_date <- study_dates[[args[[2]]]]
-  study_end_date <- study_dates[[args[[3]]]]
-  cohort <- args[[1]]
-  codelist_type <- args[[4]]
-  investigation_type <- args[[5]]
+is_being_sourced <- sys.nframe() > 0
+if (is_being_sourced == FALSE) {
+  args <- commandArgs(trailingOnly = TRUE)
+  if (length(args) == 0) {
+    study_start_date <- "2016-09-01"
+    study_end_date <- "2017-08-31"
+    cohort <- "infants"
+    codelist_type <- "sensitive"
+    investigation_type <- "primary"
+  } else {
+    study_start_date <- study_dates[[args[[2]]]]
+    study_end_date <- study_dates[[args[[3]]]]
+    cohort <- args[[1]]
+    codelist_type <- args[[4]]
+    investigation_type <- args[[5]]
+  }
 }
 covid_season_min <- as.Date("2019-09-01")
 
