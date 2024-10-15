702a1ad748b8d424c2b67b4e9fed5ef0a3d2d299
Em Prestige <emprest97@gmail.com>
2024-10-15 17:06:22 +0100

    update dummy data for infants subgroup

--- analysis/dummydata/dummydata_infants_subgroup.R
+++ analysis/dummydata/dummydata_infants_subgroup.R
@@ -50,6 +50,12 @@ sim_list = lst(
     ~ rbernoulli(n = ..n, p = 0.99)
   ),
   
+  #babys deregistration
+  deregistration_day = bn_node(
+    ~ as.integer(runif(n = ..n, index_day, index_day + 365)),
+    missing_rate = ~ 0.99
+  ),
+  
   #sex of the patient
   sex = bn_node(
     ~ rfactor(n = ..n, levels = c("female", "male", "intersex", "unknown"),
