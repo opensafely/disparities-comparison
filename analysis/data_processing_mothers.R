9110a95fae51c33e67ccd420aa47c6ab3de4ed7e
Em Prestige <emprest97@gmail.com>
2024-10-16 15:32:58 +0100

    Update data_processing_mothers.R

--- analysis/data_processing_mothers.R
+++ analysis/data_processing_mothers.R
@@ -15,15 +15,13 @@ args <- commandArgs(trailingOnly = TRUE)
 if (length(args) == 0) {
   study_start_date <- as.Date("2022-09-01")
   study_end_date <- as.Date("2023-08-31")
-  cohort <- "infants_subgroup"
   codelist_type <- "specific"
   investigation_type <- "primary"
 } else {
-  study_start_date <- study_dates[[args[[2]]]]
-  study_end_date <- study_dates[[args[[3]]]]
-  cohort <- args[[1]]
-  codelist_type <- args[[4]]
-  investigation_type <- args[[5]]
+  study_start_date <- study_dates[[args[[1]]]]
+  study_end_date <- study_dates[[args[[2]]]]
+  codelist_type <- args[[3]]
+  investigation_type <- args[[4]]
 }
 covid_season_min <- as.Date("2019-09-01")
 covid_current_vacc_min = as.Date("2020-09-01", "%Y-%m-%d")
@@ -31,13 +29,14 @@ covid_prior_vacc_min = as.Date("2021-09-01", "%Y-%m-%d")
 
 #import data
 df_input <- read_feather(
-  here::here("output", "data", paste0("input_", cohort, "_", year(study_start_date),
-                                      "_", year(study_end_date), "_", codelist_type,
+  here::here("output", "data", paste0("input_infants_subgroup_", 
+                                      year(study_start_date), "_", 
+                                      year(study_end_date), "_", codelist_type,
                                       "_", investigation_type,".arrow")))
 
 #select the variables of interest
 df_input <- df_input %>%
-  sort(birth_date, decreasing = FALSE) %>%
+  arrange(birth_date) %>%
   select(patient_id = mother_id, index_date = birth_date) %>%
   group_by(patient_id) %>%
   slice_head(n = 1) %>%
@@ -48,6 +47,6 @@ fs::dir_create(here("output", "data"))
 
 #write the new input file
 write_feather(df_input, here::here("output", "data", paste0("input_mothers_processed_", 
-                                   cohort, "_", year(study_start_date), "_", 
-                                   year(study_end_date), "_", codelist_type, 
-                                   "_", investigation_type, ".arrow")))
+                                   year(study_start_date), "_", year(study_end_date),
+                                   "_", codelist_type, "_", investigation_type,
+                                   ".arrow")))
