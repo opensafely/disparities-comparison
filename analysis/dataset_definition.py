29a629d882bd30bd211b2d13b0b9e6e4a5a60f5a
Em Prestige <emprest97@gmail.com>
2024-10-22 14:30:23 +0100

    Update dataset_definition.py

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -248,7 +248,7 @@ care_home_tpp = (
 care_home_code = (has_prior_event(codelists.carehome_codelist))
 care_home = care_home_tpp | care_home_code
 
-if cohort "infants_subgroup" :
+if cohort == "infants_subgroup" :
   mother_id_present = parents.mother_id.is_not_null()
 
 ##define populations
