c4649d833ccc3d549d54867e432179fe5ce8f0b0
Em Prestige <emprest97@gmail.com>
2024-10-22 11:09:42 +0100

    Update dataset_definition.py

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -65,6 +65,7 @@ else :
 #set index date (and registration date) as last date of either start date or age date
 #so that patients are the correct age for the cohort when looking at records
 index_date = maximum_of(study_start_date, age_date)
+dataset.patient_index_date = index_date
 registration_date = index_date - years(1)
 
 #define patients status: alive/dead
