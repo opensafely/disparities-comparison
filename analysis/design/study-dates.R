e2b98922ca050e36a5365fc207210dc834dda89e
Em Prestige <emprest97@gmail.com>
2024-10-28 13:52:44 +0000

    add eighth season

--- analysis/design/study-dates.R
+++ analysis/design/study-dates.R
@@ -18,18 +18,20 @@ study_dates <- tibble::lst(
   study_end_date = ymd("2024-01-28"), # last study entry dates
   season1_start_date = ymd("2016-09-01"), #start of first season
   season1_end_date = ymd("2017-08-31"), #end of first season
-  season2_start_date = ymd("2017-09-01"), #start of first season
-  season2_end_date = ymd("2018-08-31"), #end of first season
-  season3_start_date = ymd("2018-09-01"), #start of first season
-  season3_end_date = ymd("2019-08-31"), #end of first season
-  season4_start_date = ymd("2019-09-01"), #start of first season
-  season4_end_date = ymd("2020-08-31"), #end of first season
-  season5_start_date = ymd("2020-09-01"), #start of first season
-  season5_end_date = ymd("2021-08-31"), #end of first season
-  season6_start_date = ymd("2021-09-01"), #start of first season
-  season6_end_date = ymd("2022-08-31"), #end of first season
-  season7_start_date = ymd("2022-09-01"), #start of first season
-  season7_end_date = ymd("2023-08-31") #end of first season
+  season2_start_date = ymd("2017-09-01"), #start of second season
+  season2_end_date = ymd("2018-08-31"), #end of second season
+  season3_start_date = ymd("2018-09-01"), #start of third season
+  season3_end_date = ymd("2019-08-31"), #end of third season
+  season4_start_date = ymd("2019-09-01"), #start of fourth season
+  season4_end_date = ymd("2020-08-31"), #end of fourth season
+  season5_start_date = ymd("2020-09-01"), #start of fifth season
+  season5_end_date = ymd("2021-08-31"), #end of fifth season
+  season6_start_date = ymd("2021-09-01"), #start of sixth season
+  season6_end_date = ymd("2022-08-31"), #end of sixth season
+  season7_start_date = ymd("2022-09-01"), #start of seventh season
+  season7_end_date = ymd("2023-08-31"), #end of seventh season
+  season8_start_date = ymd("2023-09-01"), #start of eighth season
+  season8_end_date = ymd("2024-01-28") #end of eighth season
 )
 
 jsonlite::write_json(study_dates, path = here::here("analysis",
