9c57131d17a3542256427e6562929912333c1383
Em Prestige <emprest97@gmail.com>
2025-01-15 10:36:51 +0000

    Update event_count.R

--- analysis/functions/event_count.R
+++ analysis/functions/event_count.R
@@ -52,6 +52,7 @@ group_specific_events <- function(df, add_characteristics,
     
     #add column with whether there are enough events
     events_per_group <- events_per_group %>%
+      group_by(group) %>%
       mutate(enough_events_mild = if_else(sum(events_mild > 0) > 1,
                                           TRUE, FALSE),
              enough_events_severe = if_else(sum(events_mild > 0) > 1,
