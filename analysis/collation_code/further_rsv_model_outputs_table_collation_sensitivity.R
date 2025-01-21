88df665d300c0a552c4b3b870aad0f363a70ccae
Em Prestige <emprest97@gmail.com>
2025-01-21 10:00:28 +0000

    update collation code

---
+++ analysis/collation_code/further_rsv_model_outputs_table_collation_sensitivity.R
@@ -0,0 +1,41 @@
+library(tidyverse)
+library(here)
+library(arrow)
+
+#define cohort
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 0) {
+  cohort <- "adults"
+} else {
+  cohort <- args[[1]]
+}
+
+## create output directories ----
+fs::dir_create(here::here("output", "collated", "analytic", "sensitivity"))
+
+##model outputs
+
+#import rsv results table by cohort 
+#(sensitivity investigation, specific phenotypes)
+collated_model_outputs_rsv_further = rbind(
+  read_csv(here::here("output", "results", "models", "rsv_sensitivity", 
+                      paste0("further_rsv_ethnicity_model_outputs_", 
+                             cohort, "_2018_2019_specific_sensitivity.csv"))) 
+  %>% mutate(model_type = "ethnicity", codelist_type = "specific", 
+             investigation_type = "sensitivity", subset = "2018_19"),
+  read_csv(here::here("output", "results", "models", "rsv_sensitivity", 
+                      paste0("further_rsv_ses_model_outputs_", 
+                             cohort, "_2018_2019_specific_sensitivity.csv"))) 
+  %>% mutate(model_type = "ses", codelist_type = "specific", 
+             investigation_type = "sensitivity", subset = "2018_19"),
+  read_csv(here::here("output", "results", "models", "rsv_sensitivity", 
+                      paste0("further_rsv_ethnicity_ses_model_outputs_", 
+                             cohort, "_2018_2019_specific_sensitivity.csv"))) 
+  %>% mutate(model_type = "ethnicity_ses", codelist_type = "specific", 
+             investigation_type = "sensitivity", subset = "2018_19")
+)
+
+#save as csv
+write_csv(collated_model_outputs_rsv_further, 
+          paste0(here::here("output", "collated", "analytic", "sensitivity"),
+                 "/", cohort, "_further_rsv_model_outputs_collated_sensitivity.csv"))
