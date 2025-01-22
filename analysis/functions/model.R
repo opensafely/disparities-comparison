468bfd3ddfab0e1ad3e3077926f78ed44ca85cb0
Em Prestige <emprest97@gmail.com>
2025-01-22 11:53:22 +0000

    Update model.R

--- analysis/functions/model.R
+++ analysis/functions/model.R
@@ -9,38 +9,38 @@ fs::dir_create(here::here("analysis", "functions"))
 #create function for poisson regression
 glm_poisson <- function(df, x, y, offset_var) {
   
-  #reformat variables
-  x <- syms(x)
-  x_combined <- paste0(map_chr(x, as_string), collapse = " + ")
+  #define the base predictors
+  predictors <- c(x, "age_band", "sex")
   
-  #dynamically construct the formula
-  formula <- as.formula(paste0(y, " ~ ", x_combined,
-                               " + age_band + sex + offset(log(",
-                               offset_var, " * 1000))"))
-  
-  
-  #create model
+  #update predictors based on cohort and investigation type
   if (cohort == "older_adults" & investigation_type == "secondary") {
     
-    #update the formula with relevant additional variables
-    formula <- update(formula, . ~ . + has_asthma + has_copd +
-                        has_cystic_fibrosis + has_other_resp + has_diabetes +
-                        has_addisons + severe_obesity + has_chd + has_ckd +
-                        has_cld + has_cnd + has_cancer + immunosuppressed + 
-                        has_sickle_cell + smoking_status + hazardous_drinking +
-                        drug_usage)
-    
-    #run model
-    model <- glm(formula, data = df, family = poisson)
-    
-  } else {
-    
-    #run model
-    model <- glm(formula, data = df, family = poisson)
+    additional_predictors <- c("has_asthma", "has_copd", "has_cystic_fibrosis", 
+                               "has_other_resp", "has_diabetes", "has_addisons", 
+                               "severe_obesity", "has_chd", "has_ckd", 
+                               "has_cld", "has_cnd", "has_cancer", 
+                               "immunosuppressed", "has_sickle_cell", 
+                               "smoking_status", "hazardous_drinking",
+                               "drug_usage")
+    predictors <- c(predictors, additional_predictors)
     
   }
   
-  #tidy
+  #add offset to the formula
+  offset_term <- paste0("offset(log(", offset_var, " * 1000))")
+  
+  #construct the formula as a string
+  formula_string <- paste(
+    y, "~", paste(c(predictors, offset_term), collapse = " + ")
+  )
+  
+  #convert to a formula object
+  formula <- as.formula(formula_string)
+  
+  #fit the model
+  model <- glm(formula, data = df, family = poisson)
+  
+  #tidy model output
   tidy_model <- tidy(model, conf.int = TRUE)
   
   #return output
@@ -50,104 +50,63 @@ glm_poisson <- function(df, x, y, offset_var) {
 
 #create function for poisson regression with further adjustment
 glm_poisson_further <- function(df, x, y, prior_vacc, vacc_mild,
-                                vacc_severe, offset) {
+                                vacc_severe, offset_var) {
   
   #define minimum dates for covid seasons
   covid_season_min <- as.Date("2019-09-01")
   covid_current_vacc_min = as.Date("2020-09-01", "%Y-%m-%d")
   covid_prior_vacc_min = as.Date("2021-09-01", "%Y-%m-%d")
   
-  #reformat variables
-  x <- syms(x)
-  x_combined <- paste0(map_chr(x, as_string), collapse = " + ")
+  #combine predictors
+  predictors <- c(x, "age_band", "sex", "rurality_classification")
   
-  #dynamically construct the formula
-  formula <- as.formula(paste0(y, " ~ ", x_combined,
-                               " + age_band + sex + rurality_classification",
-                               " + offset(log(", offset_var, " * 1000))"))
+  #add offset to the formula
+  offset_term <- paste0("offset(log(", offset_var, " * 1000))")
   
-  #create model
+  #update predictors based on the outcome, cohort, and study start date
   if (cohort == "infants_subgroup") {
     
-    #update the formula with relevant additional variables
-    formula <- update(formula, . ~ . + maternal_age + maternal_smoking_status +
-                        maternal_drinking + maternal_drug_usage +
-                        maternal_flu_vaccination +
-                        maternal_pertussis_vaccination)
-    
-    #run model
-    model <- glm(formula, data = df, family = poisson)
+    maternal <- c("maternal_age", "maternal_smoking_status",
+                  "maternal_drinking", "maternal_drug_usage",
+                  "maternal_flu_vaccination",
+                  "maternal_pertussis_vaccination")
+    predictors <- c(predictors, maternal)
     
   } else if (cohort == "infants") {
     
-    #run model
-    model <- glm(formula, data = df, family = poisson)
+    next
     
   } else {
     
-    if (grepl("flu_primary", y)) {
+    if (y == "flu_primary_inf") {
       
-      #update the formula with relevant additional variables
-      formula <- update(formula, . ~ . + prior_vacc + vacc_mild)
+      predictors <- c(predictors, prior_vacc, vacc_mild)
       
-      #run model
-      model <- glm(formula, data = df, family = poisson)
+    } else if (y == "flu_secondary_inf") {
       
-    } else if (grep("flu_secondary", y)) {
+      predictors <- c(predictors, prior_vacc, vacc_severe)
       
-      #update the formula with relevant additional variables
-      formula <- update(formula, . ~ . + prior_vacc + vacc_severe)
+    } else if (y == "covid_primary_inf") {
       
-      #run model
-      model <- glm(formula, data = df, family = poisson)
-      
-    } else if (grepl("covid_primary", y)) {
-      
-      if (study_start_date < covid_current_vacc_min) {
+      if (study_start_date == as.Date("2020-09-01")) {
         
-        #run model
-        model <- glm(formula, data = df, family = poisson)
+        predictors <- c(predictors, vacc_mild)
         
-      } else if (study_start_date == covid_current_vacc_min) {
+      } else if (study_start_date >= as.Date("2021-09-01")) {
         
-        #update the formula with relevant additional variables
-        formula <- update(formula, . ~ . + vacc_mild)
-        
-        #run model
-        model <- glm(formula, data = df, family = poisson)
-        
-      } else {
-        
-        #update the formula with relevant additional variables
-        formula <- update(formula, . ~ . + prior_vacc + vacc_mild)
-        
-        #run model
-        model <- glm(formula, data = df, family = poisson)
+        predictors <- c(predictors, prior_vacc, vacc_mild)
         
       }
       
-    } else if (grepl("covid_secondary", y)) {
+    } else if (y == "covid_secondary_inf") {
       
-      if (study_start_date < covid_current_vacc_min) {
+      if (study_start_date == as.Date("2020-09-01")) {
         
-        #run model
-        model <- glm(formula, data = df, family = poisson)
+        predictors <- c(predictors, vacc_severe)
         
-      } else if (study_start_date == covid_current_vacc_min) {
+      } else if (study_start_date >= as.Date("2021-09-01")) {
         
-        #update the formula with relevant additional variables
-        formula <- update(formula, . ~ . + vacc_severe)
-        
-        #run model
-        model <- glm(formula, data = df, family = poisson)
-        
-      } else {
-        
-        #update the formula with relevant additional variables
-        formula <- update(formula, . ~ . + prior_vacc + vacc_severe)
-        
-        #run model
-        model <- glm(formula, data = df, family = poisson)
+        predictors <- c(predictors, prior_vacc, vacc_severe)
         
       }
       
@@ -155,7 +114,19 @@ glm_poisson_further <- function(df, x, y, prior_vacc, vacc_mild,
     
   }
   
-  #tidy
+  #construct the formula as a string
+  formula_string <- paste(
+    y, "~", paste(predictors, collapse = " + "),
+    "+ offset(log(", offset_var, "* 1000))"
+  )
+  
+  #convert to a formula object
+  formula <- as.formula(formula_string)
+  
+  #fit the model
+  model <- glm(formula, data = df, family = poisson)
+  
+  #tidy model output
   tidy_model <- tidy(model, conf.int = TRUE)
   
   #return output
