3b04162ab457e258a802994f0d295a799e0cabd5
Em Prestige <emprest97@gmail.com>
2025-04-10 15:54:39 +0100

    Update rolling_rates.R

--- post_check/functions/rolling_rates.R
+++ post_check/functions/rolling_rates.R
@@ -76,7 +76,9 @@ create_rolling_plots <- function(season, phenotype) {
             '#aec7e8', '#ff7f0e')
   )
   
-  plots <- for (characteristic in characteristics) {
+  plots <- list()
+  
+  for (characteristic in characteristics) {
     
     col <- cols %>%
       filter(var == characteristic) %>%
@@ -86,36 +88,34 @@ create_rolling_plots <- function(season, phenotype) {
       filter(characteristic == !!characteristic)
     alpha_length <- length(unique(alpha_length$group))
     
+    pathogen_print <- case_when(
+      pathogen == "rsv" ~ "RSV",
+      pathogen == "flu" ~ "Influenza",
+      pathogen == "covid" ~ "COVID-19",
+      pathogen == "overall_resp" ~ "Overall Respiratory"
+    )
+    
+    df <- df %>%
+      mutate(
+        outcome = case_when(
+          event == paste0(pathogen, "_primary_date") ~
+            paste0("Mild ", pathogen_print),
+          event == paste0(pathogen, "_secondary_date") ~
+            paste0("Severe ", pathogen_print),
+        ),
+      )
+    
     options(scipen = 999)
     
-    for (outcome in outcomes) {
-      
-      plot <- df %>%
-        filter(event == !!outcome, characteristic == !!characteristic) %>%
-        ggplot(aes(x = interval_start, y = rate_1000_py_midpoint10_derived,
-                   group = group, alpha = group)) +
-        geom_line(stat = "smooth", method = "loess", col = col, lwd = 1,
-                  span = 0.25, se = FALSE) + theme_bw() +
-        labs(x = "Date", y = "Rate per 1000 person-years\n(Midpoint 10 Derived)",
-             alpha = "Group") + scale_alpha_manual(values = c(
-               seq(1, 0.25, length.out = alpha_length))) +
-        ggtitle(paste0("30-Day Rolling Rate of ", str_to_title(gsub("_", " ",
-                outcome)), " by ", str_to_title(gsub("_", " ", characteristic)),
-                " (", str_to_title(phenotype), " Phenotype)"))
-      
-      print(plot)
-      
-      saveas <- paste0("30-Day Rolling Rate of ", str_to_title(gsub("_", " ",
-                       outcome)), " by ", str_to_title(gsub("_", " ",
-                       characteristic)), " (", str_to_title(phenotype),
-                       " Phenotype)")
-      
-      ggsave(here("post_check", "plots", "primary_analyses",
-             "seasons_of_interest", "weekly", cohort,
-             paste0(str_to_title(cohort), "_", saveas, ".png")),
-             plot, height = 8, width = 15)
-      
-    }
+    plots[[characteristic]] <- df %>%
+      filter(characteristic == !!characteristic) %>%
+      ggplot(aes(x = interval_start, y = rate_1000_py_midpoint10_derived,
+                 group = group, alpha = group)) +
+      geom_line(stat = "smooth", method = "loess", col = col, lwd = 1,
+                span = 0.25, se = FALSE) + theme_bw() +
+      labs(x = "", y = "",
+           alpha = "Group") + scale_alpha_manual(values = c(
+           seq(1, 0.25, length.out = alpha_length))) + facet_wrap( ~ outcome)
     
   }
   
@@ -124,92 +124,40 @@ create_rolling_plots <- function(season, phenotype) {
 }
 
 #helper function to create and save plots
-create_rolling_plots_overall <- function(season, phenotype) {
-
-  start <- year(study_dates[[paste0(season, "_start_date")]])
-  end <- year(study_dates[[paste0(season, "_end_date")]])
+create_rolling_plots_overall <- function(df, pathogen, phenotype) {
   
-  outcomes <- c(paste0(pathogen, "_primary_date"),
-                paste0(pathogen, "_secondary_date"))
+  cols <- scales::seq_gradient_pal(
+    "#F05039", "#1F449c", "Lab")(seq(0,1,length.out=8))
+  cols <- c(cols[2], cols[3], cols[5], cols[8])
   
-  df <- read_csv(here::here("post_check", "output", "results", "rates",
-                 "weekly", paste0("rates_over_time_", pathogen, "_",
-                 cohort, "_", start, "_", end, "_", phenotype, "_",
-                 investigation_type, ".csv")))
-  
-  df <- df %>%
-    mutate(
-      characteristic = case_when(
-        group %in% c("0-2m", "3-5m", "6-11m", "12-23m", "65-74y",
-                     "75-89y", "90y+") ~ "Age Group",
-        group %in% c("Female", "Male") ~ "Sex",
-        group %in% c("White", "Mixed", "Asian or Asian British",
-                     "Black or Black British", "Other Ethnic Groups",
-                     "Unknown") ~ "Ethnicity",
-        group %in% c("1 (least deprived)", "2", "3", "4",
-                     "5 (most deprived)") ~ "IMD Quintile",
-        group %in% c("Multiple of the Same Generation", "Living Alone",
-                     "One Other Generation", "Two Other Generations",
-                     "Three Other Generations") ~ "Household Composition",
-        group %in% c("Urban Major Conurbation", "Urban Minor Conurbation",
-                     "Urban City and Town", "Rural Town and Fringe",
-                     "Rural Village and Dispersed") ~ "Rurality"
-      )
-    )
-  
-  characteristics <- unique(df$characteristic)
-  
-  df <- df %>%
-    group_by(characteristic, interval, event) %>%
-    mutate(
-      events = sum(total_events_midpoint10),
-      rate_py = sum(rate_1000_py_midpoint10_derived),
-      rate = sum(rate_midpoint10_derived)
-    )
-  
-  cols <- tibble(
-    var = c("Age Group", "Sex", "Ethnicity", "IMD Quintile",
-            "Household Composition", "Rurality"),
-    col = c('#1f77b4', '#ffbb78', '#2ca02c', '#ff9896',
-            '#aec7e8', '#ff7f0e')
+  pathogen_print <- case_when(
+    pathogen == "rsv" ~ "RSV",
+    pathogen == "flu" ~ "Influenza",
+    pathogen == "covid" ~ "COVID-19",
+    pathogen == "overall_resp" ~ "Overall Respiratory"
   )
   
-  plots <- for (characteristic in characteristics) {
-    
-    col <- cols %>%
-      filter(var == characteristic) %>%
-      pull(col)
-    
-    options(scipen = 999)
-    
-    for (outcome in outcomes) {
-      
-      plot <- df %>%
-        filter(event == !!outcome, characteristic == !!characteristic) %>%
-        ggplot(aes(x = interval_start, y = rate_py)) +
-        geom_line(stat = "smooth", method = "loess", span = 0.25, se = FALSE,
-                  col = col, lwd = 1) + theme_bw() +
-        labs(x = "Date", y = "Rate per 1000 person-years\n(Midpoint 10 Derived)",
-             col = "Group") + 
-        ggtitle(paste0("30-Day Rolling Rate of ", str_to_title(gsub("_", " ",
-                outcome)), " All Categories in ", characteristic, " (",
-                str_to_title(phenotype), " Phenotype)"))
-      
-      print(plot)
-      
-      saveas <- paste0("30-Day Rolling Rate of ", str_to_title(gsub("_", " ",
-                       outcome)), " All Categories in ", characteristic, " (",
-                       str_to_title(phenotype), " Phenotype)")
-      
-      ggsave(here("post_check", "plots", "primary_analyses",
-             "seasons_of_interest", "weekly", cohort,
-             paste0(str_to_title(cohort), "_", saveas, ".png")),
-             plot, height = 8, width = 15)
-      
-    }
-    
-  }
+  df <- df %>%
+    mutate(
+      outcome = case_when(
+        event == paste0(pathogen, "_primary_date") ~
+          paste0("Mild ", pathogen_print),
+        event == paste0(pathogen, "_secondary_date") ~
+          paste0("Severe ", pathogen_print),
+      ),
+    )
   
-  return(plots)
+  options(scipen = 999)
+  
+  plot <- df %>%
+    ggplot(aes(x = interval_start, y = rate_py)) +
+    geom_line(stat = "smooth", method = "loess", span = 0.25, se = FALSE,
+              col = cols, lwd = 1, alpha = codelist_type) + theme_bw() +
+    labs(x = "Date", y = "Rate per 1000 person-years\n(Midpoint 10 Derived)",
+         col = "Season", alpha = "Phenotype") + facet_wrap(~ outcome, ncol = 2) +
+    ggtitle(paste0("30-Day Rolling Rate of ", str_to_title(gsub("_", " ",
+            pathogen_print))))
+  
+  return(plot)
   
 }
