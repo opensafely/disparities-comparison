e0b95448b3738809785be4dcf4aeb02d27e4f2f8
Em Prestige <emprest97@gmail.com>
2025-05-08 14:55:10 +0100

    Update tmerge_alt.R

--- analysis/functions/tmerge_alt.R
+++ analysis/functions/tmerge_alt.R
@@ -47,9 +47,11 @@ tmerge_alt <- function(df, outcome) {
       outcome_event = event(outcome_event)
     ) |>
     mutate(
-      persontime = tstop - tstart + 1
+      persontime = tstop - tstart + 1,
+      persontime_years = time_length(persontime, "years")
     )
   
+  #return
   return(dat_1rpe_tmerge)
   
 }
