3223848b6a0cd5ca183eda72ca08c72d39c906e2
Em Prestige <emprest97@gmail.com>
2025-05-15 11:33:59 +0100

    Update dataset_definition_flow_chart_mothers.py

--- analysis/dataset_definition_flow_chart_mothers.py
+++ analysis/dataset_definition_flow_chart_mothers.py
@@ -41,8 +41,8 @@ class matched_patients(PatientFrame) :
 
 ##define populations
 registered_mothers = (
-  practice_registrations.for_patient_on(matched_patients
-  .index_date).exists_for_patient()
+  has_a_continuous_practice_registration_spanning(matched_patients
+  .index_date - years(1), matched_patients.index_date)
 )
 
 #extract mothers whose patient id matches those who were extracted with infants
