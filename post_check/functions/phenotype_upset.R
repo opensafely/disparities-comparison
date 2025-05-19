78c1ce36c44a5e230aac562a6ded7eda37a98790
Em Prestige <emprest97@gmail.com>
2025-05-19 15:48:20 +0100

    Update phenotype_upset.R

--- post_check/functions/phenotype_upset.R
+++ post_check/functions/phenotype_upset.R
@@ -212,6 +212,177 @@ upset_plot <- function(input, seasons) {
       )
     )
   
+  #modify upset plot code
+  NoAttBasePlot <- function (legend, size_plot_height, Main_bar_plot, Matrix_plot, 
+    hratios, Size_plot, query_legend, set_metadata, set_metadata_plots, 
+    newpage) {
+    top <- 1
+    bottom <- 100
+    if ((!is.null(legend)) && (query_legend != tolower("none"))) {
+        if (query_legend == tolower("top")) {
+            top <- 3
+            bottom <- 102
+            legend_top <- 1
+            legend_bottom <- 3
+            size_plot_height <- (size_plot_height + 2)
+        }
+        else if (query_legend == tolower("bottom")) {
+            legend_top <- 101
+            legend_bottom <- 103
+        }
+    }
+    if (is.null(set_metadata)) {
+        matrix_and_mainbar_right <- 100
+        matrix_and_mainbar_left <- 21
+        size_bar_right <- 20
+        size_bar_left <- 1
+    }
+    else if (!is.null(set_metadata)) {
+        matrix_and_mainbar_right <- set_metadata$ncols + 100
+        matrix_and_mainbar_left <- set_metadata$ncols + 21
+        size_bar_right <- set_metadata$ncols + 20
+        size_bar_left <- set_metadata$ncols + 1
+        metadata_right <- set_metadata$ncols
+        metadata_left <- 1
+    }
+    if (newpage) {
+        grid::grid.newpage()
+    }
+    if ((!is.null(legend)) && (query_legend != tolower("none"))) {
+        if (query_legend == tolower("top")) {
+            pushViewport(viewport(layout = grid.layout(102, matrix_and_mainbar_right)))
+        }
+        else if (query_legend == tolower("bottom")) {
+            pushViewport(viewport(layout = grid.layout(103, matrix_and_mainbar_right)))
+        }
+    }
+    else if ((is.null(legend)) || (query_legend == tolower("none"))) {
+        pushViewport(viewport(layout = grid.layout(100, matrix_and_mainbar_right)))
+    }
+    # Modified
+    vp = UpSetR:::vplayout(top:bottom, 1:(matrix_and_mainbar_right-matrix_and_mainbar_left))
+    pushViewport(vp)
+    grid.draw(arrangeGrob(Main_bar_plot, Matrix_plot, heights = hratios))
+    popViewport()
+    # Modified
+    vp = UpSetR:::vplayout(size_plot_height:bottom, (matrix_and_mainbar_right-matrix_and_mainbar_left-1):96)
+    pushViewport(vp)
+    grid.draw(arrangeGrob(Size_plot))
+    popViewport()
+    if (!is.null(set_metadata)) {
+        for (i in 1:length(set_metadata_plots)) {
+            if (i != 1) {
+                metadata_left <- 1 + metadata_right
+                metadata_right <- metadata_right + set_metadata$plots[[i]]$assign
+            }
+            else {
+                metadata_left <- 1
+                metadata_right <- set_metadata$plots[[i]]$assign
+            }
+            vp = UpSetR:::vplayout(size_plot_height:bottom, metadata_left:metadata_right)
+            pushViewport(vp)
+            grid.draw(arrangeGrob(set_metadata_plots[[i]]))
+            popViewport()
+        }
+    }
+    if ((!is.null(legend)) && (query_legend != tolower("none"))) {
+        vp = UpSetR:::vplayout(legend_top:legend_bottom, matrix_and_mainbar_left:matrix_and_mainbar_right)
+        pushViewport(vp)
+        grid.draw(arrangeGrob(legend))
+        popViewport()
+    }
+  }
+  
+  label <- function(x) {
+    ifelse(x > 10000, 
+           scales::scientific_format(digits = 1)(x), 
+           x)
+  }
+
+  Make_size_plot <- function (Set_size_data, sbar_color, ratios, ylabel, scale_sets, 
+      text_scale, set_size_angles, set_size.show, set_size.scale_max, 
+      set_size.number_size) {
+      if (length(text_scale) > 1 && length(text_scale) <= 6) {
+          x_axis_title_scale <- text_scale[3]
+          x_axis_tick_label_scale <- text_scale[4]
+      }
+      else {
+          x_axis_title_scale <- text_scale
+          x_axis_tick_label_scale <- text_scale
+      }
+      if (ylabel == "Set Size" && scale_sets != "identity") {
+          ylabel <- paste("Set Size", paste0("( ", 
+              scale_sets, " )"))
+          if (scale_sets == "log2") {
+              Set_size_data$y <- log2(Set_size_data$y)
+          }
+          if (scale_sets == "log10") {
+              Set_size_data$y <- log10(Set_size_data$y)
+          }
+      }
+      if (!is.null(set_size.number_size)) {
+          num.size <- (set_size.number_size/2.845276) * x_axis_tick_label_scale
+      }
+      else {
+          num.size <- (7/2.845276) * x_axis_tick_label_scale
+      }
+      Size_plot <- (ggplot(data = Set_size_data, aes_string(x = "x", 
+          y = "y")) + geom_bar(stat = "identity", colour = sbar_color, 
+          width = 0.4, fill = sbar_color, position = "identity") + 
+          scale_x_continuous(limits = c(0.5, (nrow(Set_size_data) + 
+              0.5)), breaks = c(0, max(Set_size_data)), expand = c(0, 0)) +
+          scale_y_continuous(breaks = seq(
+            0, round_any(max(Set_size_data), 500), length.out = 3),
+            labels = label) +
+          theme(panel.background = element_rect(fill = "white"), 
+          plot.margin = unit(c(-0.11, -1.3, 0.5, 0.5), "lines"), 
+          axis.title.x = element_text(size = 8.3 * x_axis_title_scale), 
+          axis.text.x = element_text(size = 7 * x_axis_tick_label_scale, 
+              vjust = 1, hjust = 0.5), axis.line = element_line(colour = "gray0"), 
+          axis.line.y = element_blank(), axis.line.x = element_line(colour = "gray0", 
+              size = 0.3), axis.text.y = element_blank(), axis.ticks.y = element_blank(), 
+          panel.grid.minor = element_blank(), panel.grid.major = element_blank()) + 
+          xlab(NULL) + ylab(ylabel) + coord_flip())
+      if (set_size.show == TRUE) {
+          Size_plot <- (Size_plot + geom_text(aes(label = y, vjust = 0.5, 
+              hjust = 1.2, angle = set_size_angles), size = num.size))
+      }
+      if (scale_sets == "log10") {
+          if (!is.null(set_size.scale_max)) {
+              Size_plot <- (Size_plot + scale_y_continuous(limits = c(set_size.scale_max, 
+                  0), trans = log10_reverse_trans()))
+          }
+          else {
+              Size_plot <- (Size_plot + scale_y_continuous(trans = log10_reverse_trans()))
+          }
+      }
+      else if (scale_sets == "log2") {
+          if (!is.null(set_size.scale_max)) {
+              Size_plot <- (Size_plot + scale_y_continuous(limits = c(set_size.scale_max, 
+                  0), trans = log2_reverse_trans()))
+          }
+          else {
+              Size_plot <- (Size_plot + scale_y_continuous(trans = log2_reverse_trans()))
+          }
+      }
+      else {
+          if (!is.null(set_size.scale_max)) {
+              Size_plot <- (Size_plot + scale_y_continuous(limits = c(set_size.scale_max, 
+                  0), trans = "reverse"))
+          }
+          else {
+              #Modified
+              #Size_plot <- (Size_plot + scale_y_continuous(trans = "reverse"))
+          }
+      }
+      Size_plot <- ggplot_gtable(ggplot_build(Size_plot))
+      return(Size_plot)
+  }
+
+  assignInNamespace(x="NoAttBasePlot", value=NoAttBasePlot, ns="UpSetR")
+  assignInNamespace(x="Make_size_plot", value=Make_size_plot, ns="UpSetR")
+  
+  
   outcomes <- c("Mild", "Severe")
   codelists <- c("Specific", "Sensitive")
   subsets <- unique(df_plot$subset)
@@ -241,11 +412,13 @@ upset_plot <- function(input, seasons) {
         uu <- upset(fromExpression(input_expr),
                     nsets = 3,
                     keep.order = T,
-                    mb.ratio = c(0.6, 0.4),
+                    mb.ratio = c(0.8, 0.2),
                     number.angles = 0,
-                    text.scale = 1.1,
-                    point.size = 2.8,
+                    show.numbers = FALSE,
+                    text.scale = 1.2,
+                    point.size = 2,
                     line.size = 1,
+                    set_size.angles = 45,
                     empty.intersections = TRUE,
                     main.bar.color = col,
                     sets.bar.color = cols,
@@ -253,19 +426,20 @@ upset_plot <- function(input, seasons) {
         )
         
         plot[[phenotype]] <- plot_grid(
-          NULL,
-          plot_grid(NULL, uu$Sizes, nrow = 2, rel_heights = c(0.75, 0.25)),
-          NULL,
           plot_grid(uu$Main_bar, uu$Matrix, NULL, nrow = 3,
-                    rel_heights = c(3, 1, 0.45), align = 'hv'),
-          ncol = 4, align = 'hv', rel_widths = c(0.01, 0.25, 0.05, 0.75)
+                    rel_heights = c(2.75, 1.2, 0.65), align = 'hv'),
+          plot_grid(NULL, uu$Sizes, nrow = 2, rel_heights = c(0.85, 0.45)),
+          NULL,
+          ncol = 3, align = 'hv', rel_widths = c(0.75, 0.2, 0.1)
         )
         
       }
       
       plot_list[[season]] <- plot_grid(
         plot[["Specific"]],
-        plot[["Sensitive"]]
+        NULL,
+        plot[["Sensitive"]],
+        ncol = 3, rel_widths = c(0.8, 0.01, 0.8)
       )
       
     }
@@ -273,35 +447,56 @@ upset_plot <- function(input, seasons) {
     plot_label <- ggdraw() +
       draw_label(
         "Specific Phenotype",
-        x = 0.5, y = 0, hjust = 1.8, vjust = -2.5,
+        x = 0.5, y = 0, hjust = 1.9, vjust = -2.5,
         fontface = 'bold', size = 14
       ) +
       draw_label(
         "Sensitive Phenotype",
-        x = 1, y = 0, hjust = 1.75, vjust = -2.5,
+        x = 1, y = 0, hjust = 1.9, vjust = -2.5,
         fontface = 'bold', size = 14
       ) +
       theme(plot.background = element_rect(
         fill = "white", colour = "white"))
     
-    plots[[outcome]] <- plot_grid(plot_grid(
+    plots[[outcome]] <- plot_grid(
       plotlist = plot_list,
       ncol = 1, align = "v",
       axis = "tb",
       labels = gsub("_", "-", seasons),
-      label_size = 12),
-      plot_label,
-      nrow = 2, rel_heights = c(0.95, 0.05)
-    ) %>%
-    annotate_figure(
-      top = text_grob(
-        paste0("Identification of ", outcome, " Outcomes in ",
-               str_to_title(gsub("_", " ", cohort)), " by Season of Interest"),
-        color = "black", face = "bold", size = 16),
-    )
+      vjust = -0.5,
+      hjust = 0.5,
+      label_size = 12
+      )
     
   }
   
-  return(plots)
+  plot_final2 <- plot_grid(
+    NULL,
+    plot_grid(NULL, plots[["Mild"]], ncol = 2, rel_widths = c(0.05, 1)),
+    NULL,
+    plot_grid(NULL, plots[["Severe"]], ncol = 2, rel_widths = c(0.05, 1)),
+    nrow = 4,
+    rel_heights = c(0.035, 0.5, 0.02, 0.5),
+    labels = c("", "Mild", "", "Severe"),
+    label_size = 16,
+    label_fontface = "bold",
+    hjust = c(0, -0.1, 0, -0.05), 
+    vjust = -1.45
+  )
+  plot_final <- plot_grid(
+    plot_final2,
+    plot_label,
+    nrow = 2, rel_heights = c(0.95, 0.025)
+  ) %>%
+    annotate_figure(
+      top = text_grob(
+        paste0("Identification of Outcomes in ",
+               str_to_title(gsub("_", " ", cohort)),
+               " by Season of Interest"),
+        color = "black", face = "bold", size = 16, 
+        vjust = 1.25)
+    )
+  
+  return(plot_final)
   
 }
