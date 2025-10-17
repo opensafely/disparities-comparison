7ba4fe41f50bdd9a48904bde043ead87272411dc
Em Prestige <emprest97@gmail.com>
2025-10-17 15:25:34 +0100

    Update additional_comorbidities.py

--- analysis/additional_comorbidities.py
+++ analysis/additional_comorbidities.py
@@ -182,17 +182,17 @@ has_asthma = (
 copd_res = (case(
   when(last_prior_event(codelists.copd_resolved_codelist).date
   .is_on_or_after(last_prior_event(codelists.copd_codelist).date))
-  .then(True), otherwise = False)
+  .then(True),
+  when(last_prior_event(codelists.copd_resolved_codelist).date
+  .is_on_or_after(last_prior_event(codelists.copd_qof_codelist).date))
+  .then(True),
+  otherwise = False)
 )
 has_copd = (case(
-  when(last_prior_event(codelists.copd_resolved_codelist).date
-  .is_on_or_after(last_prior_event(codelists.copd_codelist).date))
-  .then(False), when(last_prior_event(codelists
-  .copd_qof_codelist).date.is_on_or_after(
-  last_prior_event(codelists.copd_codelist).date))
-  .then(False), when(((has_prior_event(codelists.copd_codelist)) 
-  | (has_prior_meds(codelists.copd_medications)) 
-  | (has_prior_event(codelists.copd_qof_codelist)))
+  when(((has_prior_event(codelists.copd_codelist)) 
+  |(has_prior_meds(codelists.copd_medications,
+    where = medications.date.is_on_or_between(medication_date, index_date))) 
+  |(has_prior_event(codelists.copd_qof_codelist)))
   & (~copd_res)).then(True), otherwise = False)
 )
   
